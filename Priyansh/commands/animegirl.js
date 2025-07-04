dd2a0c05e18bb25d9ce74baec722841d1b95ab18
botbuilderakash <aimessengerchatbot+1@gmail.com>
2025-07-04 23:33:45 +0600

    Update animegirl.js

--- Priyansh/commands/animegirl.js
+++ Priyansh/commands/animegirl.js
@@ -3,9 +3,9 @@ module.exports.config = {
   version: "1.0.0",
   hasPermssion: 0,
   credits: "𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡",
-  description: "Random Loli Pics",
-  commandCategory: "nsfw",
-  usages: "lolilewd",
+  description: "Random Anime pics",
+  commandCategory: "animepic",
+  usages: "animegirl",
   cooldowns: 5,
   dependencies: {
     "request":"",
@@ -106,7 +106,7 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.ibb.co/Ndh665cy/Yor-Forger-Spy-x-Family.jpg",
      
     ];
-      var callback = () => api.sendMessage({body:`Random Loli Pic\nNumber of photos: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID);  
+      var callback = () => api.sendMessage({body:`a random anime girl.These images are collected from the internet by the bot owner 𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡\nNumber of photos: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID);  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback()); 
     
 
