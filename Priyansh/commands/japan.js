ef92a7be0b4fa31a383b02377160c81d5cbfca84
botbuilderakash <aimessengerchatbot+1@gmail.com>
2025-07-07 00:45:24 +0600

    Update japan.js

--- Priyansh/commands/japan.js
+++ Priyansh/commands/japan.js
@@ -2,7 +2,7 @@
   name: "japan",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡",
   description: "Random ảnh girl",
   commandCategory: "random-img",
   usages: "japan",
