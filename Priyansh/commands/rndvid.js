943281796c3265b95d5f74bf26c187a8b36d66c3
botbuilderakash <aimessengerchatbot+1@gmail.com>
2025-07-07 01:10:05 +0600

    Update rndvid.js

--- Priyansh/commands/rndvid.js
+++ Priyansh/commands/rndvid.js
@@ -1,34 +1,35 @@
+const fs = require("fs-extra");
+const axios = require("axios");
+const request = require("request");
 
 module.exports.config = {
   name: "rndvid",
-  version: "1.0.0",
-  hasPermssion: 0,
+  version: "1.1",
+  hasPermission: 0,
   credits: "𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡",
-  description: "Random videos",
-  commandCategory: "videos",
-  usages: "randomvideos",
-  cooldowns: 5,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-    
+  description: "Send random video",
+  commandCategory: "media",
+  usages: "/rndvid",
+  cooldowns: 10
 };
 
-module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-  var link = [
+const videoLinks = [
+  "https://files.catbox.moe/lkqead.mp4",
+  "https://files.catbox.moe/jdjphr.mp4",
+  "https://files.catbox.moe/roo7ya.mp4",
+  "https://files.catbox.moe/jzzzhf.mp4"
+];
 
-"https://drive.google.com/file/d/17kZlyMag6Xs9dN3QWNI-H5gH-vBAvLwf/view",
-"https://drive.google.com/file/d/17kZlyMag6Xs9dN3QWNI-H5gH-vBAvLwf/view"
-     
-    ];
-      var callback = () => api.sendMessage({body:`Random videos.These videos are collected from the internet by the bot owner 𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡\nNumber of videos: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"), event.messageID);  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp4")).on("close",() => callback()); 
-    
+module.exports.run = async ({ api, event }) => {
+  const link = videoLinks[Math.floor(Math.random() * videoLinks.length)];
+  const filePath = __dirname + "/cache/rndvid.mp4";
 
-}
-  
+  const callback = () => {
+    api.sendMessage({
+      body: `🎥 এই ভিডিওগুলো 𝐀𝐡𝐚𝐬𝐚𝐧𝐮𝐥 𝐇𝐚𝐪𝐮𝐞 𝐀𝐤𝐚𝐬𝐡 ইন্টারনেট থেকে সংগ্রহ করেছেন`,
+      attachment: fs.createReadStream(filePath)
+    }, event.threadID, () => fs.unlinkSync(filePath));
+  };
+
+  request(link).pipe(fs.createWriteStream(filePath)).on("close", callback);
+};
